d6b81ac89443d14122fc2fd7492bf5f5eb22e613
Zaurbek Stark <121587026+zaurbek-stark@users.noreply.github.com>
2024-05-26 13:43:54 +0200

    switch model from llama 3 to gpt4o

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,15 +4,14 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 export const runtime = 'edge';
 
 const openai = new OpenAI({
-  apiKey: process.env.NVIDIA_API_KEY,
-  baseURL: 'https://integrate.api.nvidia.com/v1',
+  apiKey: process.env.OPENAI_API_KEY,
 })
 
 export async function POST(req: Request, res: Response) {
   const { messages } = await req.json();
 
   const response = await openai.chat.completions.create({
-    model: "meta/llama3-70b",
+    model: "gpt-4o",
     messages: [
       ...messages,
     ],
--- app/utils/processHtmlToText.ts
+++ app/utils/processHtmlToText.ts
@@ -2,9 +2,16 @@
 export const processHtmlToText = (html: string, limit: number = 2000): string => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, 'text/html');
-  const textContent = doc.body.textContent || ''; // Get the text content from the parsed HTML
-  console.log('processing textContent:', textContent);
-  const words = textContent.trim().split(/\s+/); // Split by whitespace to get words
+  let textContent = doc.body.textContent || ''; // Get the text content from the parsed HTML
+  
+  // Remove unwanted characters and patterns
+  textContent = textContent.split('(opens in a new window)')[0]; // Remove everything after (opens in a new window)
+  textContent = textContent.replace(/\\u003c\/?p/g, ''); // Remove \u003c/p
+  textContent = textContent.replace(/\\?\"static\/chunks\/[^\s]+/g, ''); // Remove \"static/chunks/...
+  textContent = textContent.replace(/\\?\$undefined\\?/g, ''); // Remove \"$undefined\"
+  
+  // Trim and split by whitespace to get words
+  const words = textContent.trim().split(/\s+/);
   
   // Limit to the first 2000 words
   if (words.length > limit) {
